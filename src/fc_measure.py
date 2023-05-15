4e5c1b9c198aa33b21282374501514b702f07a10
Максим Калинин <max@192.168.1.14>
2023-05-15 22:22:17 +0700

    add numpy cross correlation

--- src/fc_measure.py
+++ src/fc_measure.py
@@ -19,72 +19,92 @@ from abc import ABC, abstractmethod
 
 
 class ConnectivityMeasure(ABC):
-    """
-    Abstract class for connectivity measures
-
-    Arguments:
-        time_series (List[np.ndarray])
-
-    Returns:
-        correlation_matrices: np.ndarray
-    """
-    def __init__(self, time_series: List[np.ndarray]):
-        self.time_series = time_series
+    """Abstract class for connectivity measures"""
 
     @abstractmethod
-    def __call__(self):
+    def calculate(self, time_series: List[np.ndarray]) -> np.ndarray:
+        """
+        Calculate connectivity measure
+
+        Args:
+            time_series(List[np.ndarray])
+
+        Returns:
+            correlation_matrices: np.ndarray
+        """
         pass
 
 
 class PearsonCorrelation(ConnectivityMeasure):
-    """
-    Pearson correlation
-
-    Args:
-        time_series (List[np.ndarray])
-
-    Returns:
-        correlation_matrices: np.ndarray
-    """
-    def __init__(self, time_series: List[np.ndarray]):
-        super().__init__(time_series)
-
-    def __call__(self):
-        return self._calculate()
-
-    def _calculate(self):
+    """Pearson correlation"""
+    def calculate(self, time_series: List[np.ndarray]) -> np.ndarray:
         from nilearn.connectome import ConnectivityMeasure
         from sklearn.covariance import EmpiricalCovariance
 
         covariance_estimator = EmpiricalCovariance()
         connectivity_correlation = ConnectivityMeasure(kind="correlation", cov_estimator=covariance_estimator)
         
-        correlation_matrices = connectivity_correlation.fit_transform(self.time_series)
+        correlation_matrices = connectivity_correlation.fit_transform(time_series)
         return correlation_matrices
 
 
-class CrossCorrelation(ConnectivityMeasure):
-    """
-    Cross correlation
+class CrossCorrelationStatmodels(ConnectivityMeasure):
+    """Cross correlation with statmodels"""
+    def calculate(self, time_series: List[np.ndarray]) -> np.ndarray:
+        import statsmodels.api as sm
 
-    Args:
-        time_series (List[np.ndarray])
+        n_subjects = len(time_series)
+        n_regions = time_series[0].shape[1]
+        M = np.zeros((n_subjects, n_regions, n_regions))
 
-    Returns:
-        correlation_matrices: np.ndarray
-    """
+        for k in range(n_subjects):
+            for i in range(n_regions):
+                for j in range(i, n_regions):
+                    cross_corr = sm.tsa.stattools.ccf(time_series[k][:, i], time_series[k][:, j], adjusted=False)
+                    cross_corr = cross_corr[0:(len(time_series[k][:, i]) + 1)][::-1]
+                    max_cross_corr = np.max(cross_corr)
+                    M[k, i, j] = max_cross_corr
 
-    def __init__(self, time_series: List[np.ndarray]):
-        super().__init__(time_series)
+            M[k] = M[k] + M[k].T
+            np.fill_diagonal(M[k], 1)
+        return M
 
-    def __call__(self):
-        return self._calculate()
 
-    def _calculate(self):
-        n_subjects = len(self.time_series)
-        n_regions = self.time_series[0].shape[1]
-        correlation_matrices = np.zeros((n_subjects, n_regions, n_regions))
+class CrossCorrelationNumpy(ConnectivityMeasure):
+    """Cross correlation with numpy"""
+    @staticmethod
+    def _normalize_cross_correlation(cross_corr: np.ndarray,
+                                     series_1: np.ndarray,
+                                     series_2: np.ndarray) -> np.ndarray:
+        # Compute auto-correlations of each series at zero lag
+        auto_corr_1 = np.correlate(series_1, series_1, mode='valid')
+        auto_corr_2 = np.correlate(series_2, series_2, mode='valid')
 
-        for i in range(n_subjects):
-            pass
+        # Compute normalization factor
+        norm_factor = np.sqrt(auto_corr_1 * auto_corr_2)
 
+        # Normalize cross-correlation
+        normalized_cross_corr = cross_corr / norm_factor
+        return normalized_cross_corr
+
+    def calculate(self, time_series: List[np.ndarray]) -> np.ndarray:
+        import numpy as np
+
+        n_subjects = len(time_series)
+        n_regions = time_series[0].shape[1]
+        M = np.zeros((n_subjects, n_regions, n_regions))
+
+        for k in range(n_subjects):
+            for i in range(n_regions):
+                for j in range(i, n_regions):
+                    cross_corr = np.correlate(time_series[k][:, i], time_series[k][:, j], mode='full')
+                    normalized_cross_corr = self._normalize_cross_correlation(cross_corr,
+                                                                              time_series[k][:, i],
+                                                                              time_series[k][:, j])
+                    max_cross_corr = np.max(abs(normalized_cross_corr))
+                    M[k, i, j] = max_cross_corr
+
+            M[k] = M[k] + M[k].T
+            np.fill_diagonal(M[k], 1)
+
+        return M
